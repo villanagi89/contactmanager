fd3090bf479862eac55f13dd091b1c8789ed9b4b
Gina Villarreal <gvillarreal@blueport.com>
2018-12-13 09:39:01 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Contact from './components/Contact';
+import Header from './components/Header';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header branding="Contact Manger" />
+        <Contact name="John Doe" email="gmail" phone="1212121" />
+        <Contact name="Jane Doe" email="hotmail" phone="111111" />
       </div>
     );
   }
